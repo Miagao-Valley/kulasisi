9b5297d4ac1e3a5abab062ff958c47a9f883c4c5
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2024-10-01 22:05:49 +0800

    Create models for text entries and languages

--- backend/content/admin.py
+++ backend/content/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+
+from .models import Language, TextEntry
+
+
+admin.site.register(Language)
+admin.site.register(TextEntry)
--- backend/content/models.py
+++ backend/content/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+
+
+class Language(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        unique_together = ("code", "name")
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
+class TextEntry(models.Model):
+    content = models.TextField()
+    lang = models.ForeignKey(Language, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("content", "lang")
+
+    def __str__(self):
+        return f"{self.content} ({self.lang.code})"
