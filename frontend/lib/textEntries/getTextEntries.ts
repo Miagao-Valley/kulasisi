f43207e619f2e1dc35af6fedd309571e146ed49e
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2024-10-04 01:21:56 +0800

    Update text entry caching

--- frontend/lib/textEntries/getTextEntries.ts
+++ frontend/lib/textEntries/getTextEntries.ts
@@ -2,5 +2,7 @@ import fetcher from '@/utils/fetcher';
 import { TextEntry } from '@/types';
 
 export default async function getTextEntries(): Promise<TextEntry[]> {
-  return await fetcher(`text-entries/`);
+  return await fetcher(`text-entries/`, {
+    cache: 'no-store',
+  });
 }
--- frontend/lib/textEntries/getTextEntry.ts
+++ frontend/lib/textEntries/getTextEntry.ts
@@ -2,5 +2,7 @@ import fetcher from '@/utils/fetcher';
 import { TextEntry } from '@/types';
 
 export default async function getTextEntry(id: number): Promise<TextEntry> {
-  return await fetcher(`text-entries/${id}/`);
+  return await fetcher(`text-entries/${id}/`, {
+    cache: 'no-store',
+  });
 }
