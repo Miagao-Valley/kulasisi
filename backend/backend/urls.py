3976535d198756f7c065a8807dd583c1130de3e4
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2024-10-04 23:09:53 +0800

    Add endpoints for auth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.views import CreateUserView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("api-auth/", include("rest_framework.urls")),
     path("users/", include("users.urls")),
     path("", include("content.urls")),
 ]
