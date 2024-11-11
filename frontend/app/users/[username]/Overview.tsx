b03cdc25047319b1caa036b11c84b3305967d430
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2024-11-11 12:28:30 +0800

    Add edit profile to profile page

--- frontend/app/users/[username]/Overview.tsx
+++ frontend/app/users/[username]/Overview.tsx
@@ -1,7 +1,16 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
 import { User } from '@/types';
-import { FaBirthdayCake, FaCalendar, FaLink, FaMapPin } from 'react-icons/fa';
+import {
+  FaBirthdayCake,
+  FaCalendar,
+  FaLink,
+  FaMapPin,
+  FaPen,
+} from 'react-icons/fa';
+import { useAuth } from '@/app/components/AuthProvider';
 
 interface Props {
   user: User;
@@ -9,16 +18,28 @@ interface Props {
 }
 
 export default function Overview({ user, className = '' }: Props) {
+  const auth = useAuth();
+
   const dateJoined = new Date(user.date_joined);
   const dateOfBirth = new Date(user.date_of_birth || '');
 
   return (
     <div className={`${className}`}>
-      <div>
-        <h1 className="text-2xl mb-0">
-          {user.first_name} {user.last_name}
-        </h1>
-        <span>@{user.username}</span>
+      <div className="flex gap-1">
+        <div>
+          <h1 className="text-2xl mb-0">
+            {user.first_name} {user.last_name}
+          </h1>
+          <span>@{user.username}</span>
+        </div>
+        {auth.isAuthenticated && auth.username === user.username && (
+          <Link
+            href={`/settings/?tab=profile`}
+            className="btn btn-ghost btn-sm ms-auto"
+          >
+            <FaPen /> Edit Profile
+          </Link>
+        )}
       </div>
       <p className="mt-3 mb-2">{user.bio}</p>
       <div className="flex gap-3 text-sm mb-2">
