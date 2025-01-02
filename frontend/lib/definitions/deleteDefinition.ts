4ac387dace101059f70092f7b2a3dbd1fa6de02b
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-01-02 15:11:06 +0800

    Fix naming error

--- frontend/lib/definitions/deleteDefinition.ts
+++ frontend/lib/definitions/deleteDefinition.ts
@@ -5,7 +5,7 @@ import { revalidatePath } from 'next/cache';
 import fetcher from '@/utils/fetcher';
 import getToken from '../tokens/getToken';
 
-export default async function deleteTranslation(wordId: number, id: number) {
+export default async function deleteDefinition(wordId: number, id: number) {
   await fetcher(
     `/dictionary/definitions/${id}/`,
     {
