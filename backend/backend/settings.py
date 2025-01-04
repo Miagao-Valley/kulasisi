14cc4dfcaf112e140a4c7004193bea3a086cd054
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-01-05 01:43:45 +0800

    Extend JWT token lifetime

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -178,8 +178,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "TOKEN_OBTAIN_SERIALIZER": "users.serializers.CustomTokenObtainPairSerializer",
 }
 
